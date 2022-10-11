c7159967cc17bcd020fcde5346a12d49bc30db0f
Tony Sharples <tonysharples22@me.com>
2022-10-11 10:05:36 +0100

    Update database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ import image from './controllers/image.js'
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-reticulated-96372',
-    user: 'tonysharples',
-    password: '',
-    database: 'smart-brain',
+    host: process.env.DATABASE_URL,
+    ssl: true
   },
 })
 
